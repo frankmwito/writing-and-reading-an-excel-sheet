c151054d712004adfe41f4c0aca38340d6274ed0
Frank Mwito <Mfranklyne039@gmail.com>
2024-10-04 14:45:05 +0300

    read or retrieve data from an excel file

--- Read excel file.py
+++ Read excel file.py
@@ -1,1 +1,10 @@
-# read or retrieve from an excel file
+# read or retrieve data from an excel file
+
+from openpyxl import Workbook, load_workbook
+
+wb = load_workbook("product_inventory.xlsx")
+
+sh = wb.active
+
+for row in sh.iter_rows(values_only=True):
+    print(row)
