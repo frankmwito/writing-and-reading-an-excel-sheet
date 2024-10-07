a9fb5a69a925b1d9b6a2fb13900b2deb45ee5750
Frank Mwito <Mfranklyne039@gmail.com>
2024-10-07 10:14:02 +0300

    Read and retrieve students grades in an excel file

--- Read student_grades file.py
+++ Read student_grades file.py
@@ -1,1 +1,15 @@
 # Read and retrieve students grades in an excel file
+
+from openpyxl import load_workbook
+import openpyxl
+
+workbook  = load_workbook("students_grades.xlsx")
+
+sheet = workbook.active
+
+print("Students with grades above 85: ")
+
+for row in sheet.iter_rows(min_row=2, values_only=True):
+    name, grade = row
+    if grade > 85:
+        print(f"{name}: {grade}")
