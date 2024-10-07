ad2fe2cc3b2c139a58f7ec388d2fa94233ea14cb
Frank Mwito <Mfranklyne039@gmail.com>
2024-10-07 10:57:49 +0300

    Current time spread sheet

--- Time sheet.py
+++ Time sheet.py
@@ -1,1 +1,33 @@
 # Current time spread sheet
+
+import time
+from datetime import datetime
+from openpyxl import Workbook, load_workbook
+
+def create_excel_with_datetime():
+    workbook = Workbook()
+    worksheet = workbook.active
+    
+    worksheet['A1'] = "Data"
+    worksheet['B1'] = "Timestamp"
+    worksheet['A2'] = "Example"
+    worksheet['B2'] = datetime.now().strftime("%Y-%M-%D %H:%M:%S")
+    
+    workbook.save("date_time_record.xlsx")
+    print("Excel file created with current date and time!")
+    
+def update_excel_with_last_modified():
+    workbook = load_workbook("date_time_record.xlsx")
+    worksheet = workbook.active
+    
+    worksheet['C1'] = "Last Modfied"
+    worksheet['C2'] = datetime.now().strftime("%Y-%M-%D %H:%M:%S")
+    
+    workbook.save("date_time_record.xlsx")
+    print("Excel file updated with 'Last Modified' date!")
+    
+
+create_excel_with_datetime()
+update_excel_with_last_modified()
+
+
